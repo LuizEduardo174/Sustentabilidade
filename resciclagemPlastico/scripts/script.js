7e54eab3df13507ee73213b6e75d2bddd438dc35
LuizEduardo174 <lu1z.b4tista7@gmail.com>
2025-07-31 11:44:17 -0300

    sustentabilidade
    meio do projeto

---
+++ resciclagemPlastico/scripts/script.js
@@ -0,0 +1,9 @@
+function toggleDropdown(id) {
+    const content = document.getElementById(id);
+    content.classList.toggle("d-none");
+  }
+  
+  function mensagem() {
+    alert("Parabéns por se preocupar com o meio ambiente! 🌱");
+  }
+  
